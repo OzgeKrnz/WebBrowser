45a0ae061057018abdc4642db217fdf5ac17b558
Özge Kurnaz <ozge.k60@hotmail.com>
2024-12-09 16:23:20 +0300

    Initial Commit

---
+++ WebBrowserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowserApp
+//
+//  Created by özge kurnaz on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
